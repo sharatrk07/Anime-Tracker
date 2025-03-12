bbe0c1252745a9a59e3251f8a143f516c1e7cbf9
Sharat RK <sharatrk@Sharats-Laptop.local>
2025-03-12 22:53:13 +0530

    Fix JSON decode error in firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,10 @@
+# firebase_config.py
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 import streamlit as st
 
-cred = credentials.Certificate(json.loads(st.secrets["firebase"]["firebase_service_account"]))
+cert_str = st.secrets["firebase"]["firebase_service_account"].strip()
+cred = credentials.Certificate(json.loads(cert_str))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
