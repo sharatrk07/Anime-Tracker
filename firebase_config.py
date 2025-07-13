804c23a651adb1f4a91403bbcca5b4452e26a187
sharatrk07 <121000315+sharatrk07@users.noreply.github.com>
2025-07-13 12:11:25 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
+import json
+import tempfile
 
-cred = credentials.Certificate({
+service_account_dict = {
     "type": "service_account",
     "project_id": "anime-progress-tracker",
     "private_key_id": "77b2636edbbbfa0a0fca1f16f021527f53de8e50",
     "private_key": """-----BEGIN PRIVATE KEY-----
 MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCmmVEC+Bhm6FnD
-... (keep the full key as you provided) ...
+...rest of your private key...
 -----END PRIVATE KEY-----""",
     "client_email": "firebase-adminsdk-fbsvc@anime-progress-tracker.iam.gserviceaccount.com",
     "client_id": "108088266466817496846",
@@ -16,9 +18,14 @@ MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCmmVEC+Bhm6FnD
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40anime-progress-tracker.iam.gserviceaccount.com",
     "universe_domain": "googleapis.com"
-})
+}
+
+with tempfile.NamedTemporaryFile(mode="w+", suffix=".json", delete=False) as temp_file:
+    json.dump(service_account_dict, temp_file)
+    temp_file_path = temp_file.name
 
 if not firebase_admin._apps:
+    cred = credentials.Certificate(temp_file_path)
     firebase_admin.initialize_app(cred, {
         'storageBucket': 'anime-progress-tracker.appspot.com'
     })
